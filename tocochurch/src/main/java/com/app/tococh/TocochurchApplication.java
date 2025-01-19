07b165e1d0202cc81223a9fc32fb47e67aa5165e
angmo <angmo@angmo>
2025-01-20 00:07:53 +0900

    함께가는교회 ERP 시작

---
+++ tocochurch/src/main/java/com/app/tococh/TocochurchApplication.java
@@ -0,0 +1,13 @@
+package com.app.tococh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocochurchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocochurchApplication.class, args);
+	}
+
+}
---
+++ tocochurch/src/test/java/com/app/tococh/TocochurchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tococh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocochurchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
